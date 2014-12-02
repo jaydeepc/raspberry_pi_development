3379dd62f93d1857bea9db8ecae36a66667065cb
JD <jaydeep.c1@gmail.com>
2014-12-02 02:03:03 +0000

    Modified voice

--- simple_pi_projects/light_on.py
+++ simple_pi_projects/light_on.py
@@ -6,7 +6,7 @@ engine = pyttsx.init()
 engine.setProperty('rate', 70)
 
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[13].id)
+engine.setProperty('voice', voices[14].id)
 
 USERNAME="jaydeepc@thoughtworks.com"
 PASSWORD="Password@w00m"
@@ -24,6 +24,9 @@ while True:
         newmails = int(feedparser.parse("https://" + USERNAME + ":" + PASSWORD + "@mail.google.com/gmail/feed/atom")["feed"]["fullcount"])
         if newmails > 1:
                 GPIO.output(led_green, GPIO.HIGH)
+		GPIO.output(GPIO_PIN, GPIO.LOW)
                 engine.say("You Got Mail!")
+		engine.runAndWait()
         else:
-                GPIO.output(GPIO_PIN, GPIO.HIGH)
+                GPIO.output(GPIO_PIN, GPIO.LOW)
+		GPIO.output(led_green, GPIO.LOW)
