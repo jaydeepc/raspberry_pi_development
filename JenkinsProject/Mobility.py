621256d617e40d2e344c2f3b13643a4c1a959751
JD <jaydeepc@thoughtworks.com>
2014-12-10 17:59:08 -0500

    Added Mobility

---
+++ JenkinsProject/Mobility.py
@@ -0,0 +1,55 @@
+import RPi.GPIO as gpio
+import time
+
+
+def init():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(7, gpio.OUT)
+    gpio.setup(11, gpio.OUT)
+    gpio.setup(13, gpio.OUT)
+    gpio.setup(15, gpio.OUT)
+
+
+def kill():
+    gpio.cleanup()
+
+
+def go_forward(tf):
+    init()
+    gpio.output(7, True)
+    gpio.output(13, True)
+    time.sleep(tf)
+    gpio.output(7, False)
+    gpio.output(13, False)
+    kill()
+
+
+def go_backward(tf):
+    init()
+    gpio.output(11, True)
+    gpio.output(15, True)
+    time.sleep(tf)
+    gpio.output(11, False)
+    gpio.output(15, False)
+    kill()
+
+
+def turn_right(tf):
+    init()
+    gpio.output(7, True)
+    gpio.output(15, True)
+    time.sleep(tf)
+    gpio.output(7, False)
+    gpio.output(15, False)
+    kill()
+
+
+def turn_left(tf):
+    init()
+    gpio.output(13, True)
+    gpio.output(11, True)
+    time.sleep(tf)
+    gpio.output(13, False)
+    gpio.output(11, False)
+    time.sleep(tf)
+    kill()
---
+++ JenkinsProject/test_mobility.py
@@ -0,0 +1,11 @@
+import Mobility as move
+import time
+
+sides = input("How many sides?")
+size = input("How big?")
+full_turn = .97*4
+
+for x in range(0, sides):
+    move.go_forward(size)
+    time.sleep(.5)
+    move.turn_right(full_turn/sides)
