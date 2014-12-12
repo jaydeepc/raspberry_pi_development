dc1560c696588ae6d59515a84e4aa036e9eeab0b
JD <jaydeepc@thoughtworks.com>
2014-12-12 01:23:26 -0500

    Added a user control again and again

--- JenkinsProject/user_control.py
+++ JenkinsProject/user_control.py
@@ -4,10 +4,12 @@ key_press = ""
 while key_press is not "t":
     key_press = raw_input()
     if key_press.lower() == 'w':
+        move.kill()
         move.init()
         move.go_forward(0)
-        move.kill()
     elif key_press.lower() == 's':
+        move.kill()
         move.init()
         move.go_backward(0)
-        move.kill()
+
+move.kill()
