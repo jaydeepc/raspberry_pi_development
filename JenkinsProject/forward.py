04804feb3df551bfae98aa82880d2d88ee750097
JD <jaydeepc@thoughtworks.com>
2014-12-21 01:44:33 -0500

    Improving movement

--- JenkinsProject/forward.py
+++ JenkinsProject/forward.py
@@ -1,5 +1,9 @@
 import Mobility as move
+from subprocess import check_output, call
+
+call(["mpg123", "http://www.translate.google.com/translate_tts?tl=en&q={0}&key=AIzaSyDkKEm081DUEZFXiW1pFx6gN1JHNqg1qus&".format("For how long sir?".replace(" ", "%20"))])
+length = check_output(["speech-recog.sh"])
 
 move.init()
-move.go_forward(1)
+move.go_forward(int(length))
 move.kill()
